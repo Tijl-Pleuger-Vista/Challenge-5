26da515e04563ef3618f9a6f6fcc1f3cf1958563
fertol <144384177+fertol1337@users.noreply.github.com>
2023-10-31 13:26:53 +0100

    api
    username + password

--- api/app.py
+++ api/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+from urllib.parse import urlparse, parse_qs
+import hashlib
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/auth', methods=['POST'])
+def login():
+    params = request.args
+    usr = params.get('username')
+    pas = params.get('password')
+    pas = hashlib.md5(pas.encode()).hexdigest()
+    return usr + ':' + pas
 
 if __name__ == '__main__':
     app.run()

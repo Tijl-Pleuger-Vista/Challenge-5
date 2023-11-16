c18f930a981ad09d944a60199f93c5cef64de300
IHeadBodyScript <IHeadBodyScript@gmail.com>
2023-11-16 08:49:46 +0100

    merge

--- app/assets/js/login.js
+++ app/assets/js/login.js
@@ -1,60 +1,32 @@
-function validateLoginForm(){  
-    var email=document.loginForm.name.value;  
-    var password=document.loginForm.password.value;  
-      
+function validateLoginForm(){
+    var email=document.loginForm.email.value;
+    var password=document.loginForm.password.value;
+
     if (email==null || email==""){  
-      alert("Please enter a valid email");  
-      return false; 
-
-
-
-
-
-
-
-
-
-
-    }else if(password.length<8){  
-      alert("Please enter your password.");  
-      return false;  
-      }
-    }  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      alert("your email");  
+      return false;
+  }
+    else if(password.length<8){  
+      alert("your password");  
+      return false;
+  }
+}
 
 function validateSignupForm(){  
+    var email=document.loginForm.email.value;
     var name=document.signupForm.name.value;  
     var password=document.signupForm.password.value;  
       
-    if (name==null || name==""){  
-      alert("Name can't be blank");  
+    if (email==null || email==""){  
+      alert("something@something.something");  
       return false;  
-    }else if(password.length<8){  
-      alert("Password must be at least 8 characters long, with 2 special characters and a capital letter.");  
+  }
+    else if (name==null || name==""){  
+      alert("anything");  
       return false;  
-      }  
-    }  
+  }
+    else if(password.length<8){  
+      alert("Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters");  
+      return false;  
+  }  
+}  
--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -4,57 +4,20 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
     console.log(pokemonList)
 })
 
-
 fetch(`https://pokeapi.co/api/v2/pokemon/1`)
 .then(pokemonList => pokemonList.json())
 .then(pokemonList => {
     console.log(pokemonList)
 })
 
-// function TextMe() {
-//     var s = document.getElementById("transition");
-//     s.style.zIndex = "256", s.classList.replace("splash-transition-true", "splash-transition-false"), setTimeout(() => {
-//         document.querySelectorAll("*").forEach(s => {
-//         s.classList.remove("visible")
-//         }), document.getElementById("TextMe").classList.add("visible"), s.classList.replace("splash-transition-false", "splash-transition-true")
-//     }, 1e3), setTimeout(() => {
-//         s.style.zIndex = "-256"
-//     }, 1e3)
-// }
-
-function signup(){
+function pageLogin(){
     document.querySelectorAll("*").forEach(s => {
     s.classList.remove("visible")
     })
     document.getElementById("signInDiv").classList.add("visible")}
 
-function signin(){
+function pageSignup(){
     document.querySelectorAll("*").forEach(s => {
     s.classList.remove("visible")
     })
     document.getElementById("loginDiv").classList.add("visible")}
-
-function validateformfalse(){  
-    var name=document.myform.name.value;  
-    var password=document.myform.password.value;  
-
-    if (name==null || name==""){  
-    alert("Please enter you're Username");  
-    return false;  
-    }else if(password.length<8){  
-    alert("Please enter you're password.");  
-    return false;  
-    }  
-    }  
-
-function validateformtrue(){  
-    var name=document.myform.name.value;  
-    var password=document.myform.password.value;  
-    if (name==null || name==""){  
-    alert("Name can't be blank");  
-    return false;  
-    }else if(password.length<8){  
-    alert("Password must be at least 8 characters long, with 2 special characters and a capital letter.");  
-    return false;  
-    }  
-    }  

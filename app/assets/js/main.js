79bd1818825b87e09c1ce4feea4d9d4e2ee09df1
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-11-17 01:22:50 +0100

    Js for pokedex fetch

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,14 +1,10 @@
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
-.then(pokemonList => pokemonList.json())
-.then(pokemonList => {
-    console.log(pokemonList)
-})
 
-fetch(`https://pokeapi.co/api/v2/pokemon/1`)
-.then(pokemonList => pokemonList.json())
-.then(pokemonList => {
-    console.log(pokemonList)
-})
+
+// fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+// .then(pokemonList => pokemonList.json())
+// .then(pokemonList => {
+//     console.log(pokemonList)
+// })
 
 function pageLogin(){
     document.querySelectorAll("*").forEach(s => {
@@ -21,3 +17,5 @@ function pageSignup(){
     s.classList.remove("visible")
     })
     document.getElementById("loginDiv").classList.add("visible")}
+
+

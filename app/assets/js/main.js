ec7fe8a3369a230f11cdbc60e6eb9b4a738b50e3
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-11-17 02:05:49 +0100

    Visual update

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -18,4 +18,49 @@ function pageSignup(){
     })
     document.getElementById("loginDiv").classList.add("visible")}
 
+    //
 
+    function pageHome(){
+        document.querySelectorAll("*").forEach(s => {
+        s.classList.remove("visible")
+        })
+        document.getElementById("pokeHome").classList.add("visible")}
+
+        function pageVideogames(){
+            document.querySelectorAll("*").forEach(s => {
+            s.classList.remove("visible")
+            })
+            document.getElementById("pokeVideogames").classList.add("visible")}
+        
+    
+
+    let pokedex = () => {
+    
+        localStorage.setItem('i', '0');
+        
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`)
+        .then(pokemonList => pokemonList.json())
+        .then(pokemonList => {
+            console.log(pokemonList)
+
+            function placeDiv() {
+            // console.log("hello")
+
+            var _i = localStorage.getItem('i');
+            var name = pokemonList.results[_i].name
+            // console.log(name)
+            
+            _i++;
+            localStorage.setItem('i', _i);
+            Form.innerHTML +=
+            `
+            <div class="card poke-card">${name}</div>
+            `
+            }
+        
+    let result = 0;
+    for (let i = 0; i < pokemonList.results.length; i++) {
+    result += placeDiv();
+        }
+})};
+    pokedex();

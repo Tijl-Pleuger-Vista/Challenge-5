98efb65e9057684c40e9366278e04c75dce38ea1
JeromBons <145325667+JeromBons@users.noreply.github.com>
2023-11-22 10:42:59 +0100

    api rework

--- api/functions_db.py
+++
@@ -1,9 +0,0 @@
-# find record from username
-def select_all_records_by_username(cursor, username):
-    sql = "SELECT * FROM tb_user WHERE username=?"
-    cursor.execute(sql, [username])
-    # print(cursor.fetchall())  # or use fetchone()
-    # print("\nHere is a listing of the rows in the table\n")
-    for row in cursor.execute("SELECT rowid, * FROM tb_user ORDER BY username"):
-        return row
-

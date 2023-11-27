974659efbc376d9770aa78974c80d7abeba9a6b1
JeromBons <145325667+JeromBons@users.noreply.github.com>
2023-11-27 11:07:15 +0100

    Update main.js

--- admin/main.js
+++ admin/main.js
@@ -1,6 +1,6 @@
 function BTNDelete(clicked){
     var input = document. getElementById("delete").value;
-    fetch(`http://localhost:5000/delete/${input}`)
+    fetch(`http://localhost:5000/delete/?key=${input}`)
     console.log("tried")
 }
 

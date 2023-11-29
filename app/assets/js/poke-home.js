964e399ea6ec3e8dc87572e6b6f4c20b9470d69c
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-11-29 18:54:49 +0100

    update

--- app/assets/js/poke-home.js
+++ app/assets/js/poke-home.js
@@ -138,7 +138,25 @@ let pokemarket = () => {
             <button class="button button-buy" onclick=""><div class="row"><h1>Buy    </h1></div></button>
         </div>
         `
-})}; pokemarket();
+});
+fetch(`https://pokeapi.co/api/v2/item/great-ball`)
+.then(itemList => itemList.json())
+.then(itemList => {
+    console.log(itemList)
+    var name = itemList.name;
+    var cost = itemList.cost;
+    var sprite = itemList.sprites.default;
+    marketplace.innerHTML +=
+    `
+    <div class="card poke-card-plus">
+        <img  class="p-img" src="${sprite}" alt="" style="width: 128px; height: 128px;">
+        <div class="p-name">${name}</div>
+        <div class="p-name">${cost} Poké-yen</div>
+        <button class="button button-buy" onclick=""><div class="row"><h1>Buy    </h1></div></button>
+    </div>
+    `
+})
+}; pokemarket();
 
 // function game(){
 //     placegame.innerHTML = 
